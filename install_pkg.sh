d43ed9aaa185aee22b1366968cf5c23173333b6d
caesar <x246libra@hotmail.com>
2021-09-04 23:51:56 +0800

    upgrade docker-comoose

--- install_pkg.sh
+++ install_pkg.sh
@@ -191,7 +191,7 @@ sudo usermod -aG docker caesar
 
 ## docker-compose
 ## https://docs.docker.com/compose/install/
-docker_compose_version=1.25.4
+docker_compose_version=1.29.2
 sudo curl -L "https://github.com/docker/compose/releases/download/$docker_compose_version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 

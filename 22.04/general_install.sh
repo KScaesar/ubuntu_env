d0bb6d7c436daa57051d87820c1b7349627d6ab5
ksCaesar <x246libra@htomail.com>
2023-12-06 00:36:04 +0800

    update 22.04 base_install

--- 22.04/general_install.sh
+++ 22.04/general_install.sh
@@ -2,49 +2,13 @@
 # https://www.shellcheck.net/
 # shellcheck source=/dev/null
 
-## develop tool
+## cli tool
 sudo apt update && sudo apt install --yes \
-ripgrep \
-tldr \
-htop \
-tree 
-
-tldr --update
+ripgrep
 
 ## desktop tool
 sudo apt update && sudo apt install --yes \
-kolourpaint \
-vokoscreen \
-qbittorrent \
-peek
-
-
-
-## backup
-# https://freefilesync.org/download.php
-
-install_FreeFileSync() {
-    local version="$1"
-    local tar_filename="FreeFileSync_${version}_Linux.tar.gz"
-    local extracted_filename="FreeFileSync_${version}_Install.run"
-
-    wget -P ~/Download/ "https://freefilesync.org/download/$tar_filename" 
-    tar -zxvf ~/Download/$tar_filename -C ~/Download
-    chmod +x ~/Download/$extracted_filename && ~/Download/$extracted_filename
-}
-install_FreeFileSync "13.2"
-
-## fzf
-# https://github.com/junegunn/fzf/tree/master/shells
-curl -sSL https://github.com/junegunn/fzf/releases/download/0.44.1/fzf-0.44.1-linux_amd64.tar.gz | sudo tar --no-same-owner -xz -C /usr/bin/
-curl https://raw.githubusercontent.com/junegunn/fzf/master/bin/fzf-preview.sh -o ~/.local/bin/fzf-preview.sh && chmod 755 ~/.local/bin/fzf-preview.sh
-curl https://raw.githubusercontent.com/junegunn/fzf/master/bin/fzf-tmux -o  ~/.local/bin/fzf-tmux && chmod 755 ~/.local/bin/fzf-tmux
-sudo curl https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.bash -o /etc/bash_completion.d/fzf-key-bindings.bash
-sudo curl https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.bash -o /etc/bash_completion.d/fzf-completion.bash
-
-cat <<'EOF' >>~/.bashrc
-export FZF_DEFAULT_OPTS="--multi --bind=alt-k:up,alt-j:down --bind 'home:last,end:first' --bind 'ctrl-o:execute(vim {}),ctrl-]:execute(sudo vim {})' --preview 'echo {}' --preview-window top:40%:hidden:wrap --bind 'ctrl-p:toggle-preview'"
-EOF
+qbittorrent
 
 ## bat
 # https://github.com/sharkdp/bat/releases
